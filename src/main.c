f64d10b83b8490d1f3aba0769502195bf8eb9b0c
SweetBuunn <sasatopor357@gmail.com>
2026-08-01 14:03:21 +0300

    add map, redid movment

--- src/main.c
+++ src/main.c
@@ -6,22 +6,44 @@
 
 int main(void)
 {
-
-    int playerX = 2, playerY = 3; // spawn point.
+    //--initial coordinates--
+    int playerX = 3, playerY = 1;
     int running = 1;
-
+    int doorX = 5, doorY = 11;
+    int cmap = 1;
     while (running)
     {
+
+        char (*map)[SIZE];
+
+        if (cmap == 1) // chage map and coordinate door
+        {
+            map = nmap;
+            doorX = 5;
+            doorY = 11;
+        }
+        else if (cmap == 2)
+        {
+            map = nmap2;
+            doorX = 5;
+            doorY = 0;
+        }
+
         system("clear");
 
         printf("Exit: q/Q\n");
 
+        system("clear");
         for (int x = 0; x < SIZE; x++) // print player, map, etc.
         {
             for (int y = 0; y < SIZE; y++)
             {
                 if (x == playerX && y == playerY)
+                {
                     printf("@ ");
+                }
+                else if (x == doorX && y == doorY)
+                    printf("| ");
                 else
                     printf("%c ", map[x][y]);
             }
@@ -40,52 +62,54 @@ int main(void)
         case 'W':
         case 'w':
             playerX--;
-            if (map[playerX][playerY] != '#') // check to obstacle.
+            if (map[playerX][playerY] == '#')
             {
                 playerX++;
-                playerX--;
                 break;
             }
-            playerX++;
             break;
         case 'S':
         case 's':
             playerX++;
-            if (map[playerX][playerY] != '#') // check to obstacle.
+            if (map[playerX][playerY] == '#')
             {
                 playerX--;
-                playerX++;
                 break;
             }
-            playerX--;
             break;
         case 'A':
         case 'a':
             playerY--;
-            if (map[playerX][playerY] != '#') // check to obstacle.
+            if (map[playerX][playerY] == '#')
             {
                 playerY++;
-                playerY--;
                 break;
             }
-            playerY++;
             break;
         case 'D':
         case 'd':
             playerY++;
-            if (map[playerX][playerY] != '#') // check to obstacle.
+            if (map[playerX][playerY] == '#')
             {
                 playerY--;
-                playerY++;
                 break;
             }
-            playerY--;
             break;
         case 'Q':
         case 'q':
             running = 0;
             break;
         }
+
+        if (playerX == doorX && playerY == doorY) // change map
+        {
+            if (cmap == 1)
+                cmap = 2;
+            else if (cmap == 2)
+                cmap = 1;
+        }
     }
-    printf("coordinate: x: %d, y: %d\n", playerX, playerY); // player coordinate.
+    printf("coordinate player: x: %d, y: %d\n", playerX, playerY); // player coordinate.
+    printf("door: x:%d y:%d\n", doorX, doorY);                     // door coordinate.
+    printf("map:%d\n", cmap);                                      // number map.
 }
